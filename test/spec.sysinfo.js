3ee5e3ff2cdb6b47dc7c6d6f7815553c85977551
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2019-01-17 23:07:17 +0100

    some tests

---
+++ test/spec.sysinfo.js
@@ -0,0 +1,32 @@
+const expect = require('expect.js');
+const sysinfo = require('../src/sysinfo.js');
+
+describe('sysinfo', () => {
+  describe('getUngitLatestVersion', () => {
+    it('returns the latest ungit version from npm registry', () => {
+      sysinfo.getUngitLatestVersion().then((version) => {
+        expect(version).to.be('1.4.36');
+      })
+    });
+  });
+
+  describe('getUserHash', () => {
+    it('returns the user hash', () => {
+      sysinfo.getUserHash().then((hash) => {
+        expect(hash).to.be('df9710c69934be3982ac7d3b354f4b19');
+      })
+    });
+  });
+
+  describe('getGitVersionInfo', () => {
+    it('returns the git version', () => {
+      sysinfo.getGitVersionInfo().then((version) => {
+        expect(version).to.eql({
+          requiredVersion: '>=1.8.x',
+          version: '2.14.1',
+          satisfied: true,
+        });
+      })
+    });
+  });
+})
