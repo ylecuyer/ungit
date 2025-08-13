2f2ea711a880632addf23557e84215edd602cc88
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2025-08-13 15:22:11 +0200

    add bookmarked repositories to sidebar

--- frontend/components/sidebar/sidebar.js
+++ frontend/components/sidebar/sidebar.js
@@ -2,6 +2,8 @@ import ko from 'knockout';
 import components from '/source/js/components.js';
 import sidebarTemplate from './sidebar.html?raw';
 import octicons from '@primer/octicons';
+import storage from '/source/js/storage.js';
+import { encodePath } from '../../../backend/source/address-parser.js';
 
 components.register('sidebar', () => new SidebarViewModel());
 const sidebarElement = document.createElement('template');
@@ -13,6 +15,23 @@ class SidebarViewModel {
   constructor() {
     this.githubIcon = octicons['mark-github'].toSVG()
     this.githubLink = this.githubIcon + " Github";
+    this.repos = ko.observableArray([]);
+
+    this.fetchRepositories();
+  }
+
+
+  async fetchRepositories() {
+    const storedPaths = JSON.parse(storage.getItem('repositories') || '[]');
+    const repos = storedPaths.map(path => {
+      return {
+        path: path,
+        name: path.split('/').pop(), // Extract the last part of the path as the name
+        link: `/#/repository?path=${encodePath(path)}`,
+      };
+    });
+    console.log("Fetched repositories from storage:", repos);
+    this.repos(repos);
   }
 
   updateNode(parentElement) {
