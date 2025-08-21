c9964285b58ca25f269ede06521c9a55e713b5e7
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2025-08-21 17:16:40 +0200

    delete branch test

--- tests/clicktests/branch_spec.rb
+++ tests/clicktests/branch_spec.rb
@@ -12,4 +12,23 @@ RSpec.describe '[BRANCH]' do
 
     expect(g.branches.map(&:name)).to eq(['master', 'new-branch'])
   end
+
+  it 'can delete a branch' do
+    g = init_repo_with_one_file
+    visit_git_repo
+
+    g.branch('new-branch').create
+
+    expect(page).to have_content('new-branch')
+
+    expect {
+        find('[data-aid="branch"]', text: 'new-branch').click
+        find('[data-ta-action="delete"]').click
+        expect(page).to have_content("Are you sure?")
+        find('[data-ta-action="yes"]').click
+        expect(page).to have_no_content('new-branch')
+    }.to change { g.branches.count }.by(-1)
+
+    expect(g.branches.map(&:name)).to eq(['master'])
+  end
 end
