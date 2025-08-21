6fbb6982763f6336330f49c9643ff47a2ead046d
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2025-08-21 16:04:10 +0200

    branch spec

---
+++ tests/clicktests/branch_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe '[BRANCH]' do
+  it 'can create a branch' do
+    g = init_repo_with_one_file
+    visit_git_repo
+
+    find('[data-aid="create-branch-or-tag-btn"]').click
+    find('[data-aid="new-branch-or-tag-name"]').set('new-branch')
+    expect {
+      find('[data-aid="create-branch-btn"]').click
+      expect(page).to have_content('new-branch')
+    }.to change { g.branches.count }.by(1)
+
+    expect(g.branches.map(&:name)).to eq(['master', 'new-branch'])
+  end
+end
