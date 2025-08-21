a8f9762c40059fe1f77ecff8907e6e37f70a3cc8
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2025-08-21 17:28:44 +0200

    spec delete tag

--- tests/clicktests/tag_spec.rb
+++ tests/clicktests/tag_spec.rb
@@ -12,4 +12,23 @@ RSpec.describe '[TAG]' do
 
     expect(g.tags.map(&:name)).to eq(['new-tag'])
   end
+
+  it 'can delete a tag' do
+    g = init_repo_with_one_file
+    visit_git_repo
+
+    g.add_tag('new-tag')
+
+    expect(page).to have_content('new-tag')
+
+    expect {
+      find('[data-aid="tag"]', text: 'new-tag').click
+      find('[data-ta-action="delete"]').click
+      expect(page).to have_content("Are you sure?")
+      find('[data-ta-action="yes"]').click
+      expect(page).to have_no_content('new-tag')
+    }.to change { g.tags.count }.by(-1)
+
+    expect(g.tags.map(&:name)).to eq([])
+  end
 end
