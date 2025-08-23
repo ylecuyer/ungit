f9ee1fe7b4ecdc9faf68a2d4edd6082157a60d0c
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2025-08-23 13:42:40 +0200

    move ignore tests tb

---
+++ tests/clicktests/ignore_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe '[IGNORE]' do
+    it 'Add a file to .gitignore file' do
+        skip 'Not implemented yet'
+    end
+
+    it 'Add a file to .gitignore file when .gitignore is missing' do
+        skip 'Not implemented yet'
+    end
+
+    it 'Attempts to add a file where similar name alread exist in .gitignore' do
+        skip 'Not implemented yet'
+    end
+
+    it 'Can edit .gitignore file' do
+        skip 'Not implemented yet'
+    end
+end
