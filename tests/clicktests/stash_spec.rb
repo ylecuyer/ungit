6583dfae2e1726dee814a8c1236a7ba16e1568c8
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2025-08-23 13:51:30 +0200

    move stash test tbd

---
+++ tests/clicktests/stash_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe '[STASH]' do
+  it 'stashes' do
+    skip 'Not implemented yet'
+  end
+
+  it 'deletes a stash' do
+    skip 'Not implemented yet'
+  end
+
+  it 'applies a stash' do
+    skip 'Not implemented yet'
+  end
+end
