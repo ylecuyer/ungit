0b5bb51fb1a169561ff67e31544fc35cf6d26648
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2025-08-23 13:13:48 +0200

    add coverage script

--- tests/clicktests/support/ungit.rb
+++ tests/clicktests/support/ungit.rb
@@ -1,7 +1,11 @@
 PORT = 8998 + ENV['TEST_ENV_NUMBER'].to_i
 
 ungit_pid = fork do
-  exec("./bin/ungit --port #{PORT}")
+  if ENV['COVERAGE']
+    exec("nyc --clean -r lcov -t coverage/clicktests.#{PORT} --report-dir coverage/clicktests.#{PORT} ./bin/ungit --port #{PORT}")
+  else
+    exec("./bin/ungit --port #{PORT}")
+  end
 end
 puts "Started ungit with PID #{ungit_pid}"
 
