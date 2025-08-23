9b701037ad69c3ab17e8af670714285aeb31cb48
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2025-08-23 13:52:24 +0200

    move squash tests tbd

---
+++ tests/clicktests/squash_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe '[SQUASH]' do
+  it 'squashes' do
+    skip 'Not implemented yet'
+  end
+
+  it 'squashes with conflicts' do
+    skip 'Not implemented yet'
+  end
+
+  it 'cancels a squash with conflicts' do
+    skip 'Not implemented yet'
+  end
+end
