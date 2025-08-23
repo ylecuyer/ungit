a737fd735ba6dad5378cac653a151757477b02dd
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2025-08-23 13:48:37 +0200

    move submodule tests tbd

---
+++ tests/clicktests/submodule_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe '[SUBMODULE]' do
+  it 'can add a submodule' do
+    skip 'Not implemented yet'
+  end
+
+  it 'can update a submodule' do
+    skip 'Not implemented yet'
+  end
+
+  it 'can move into a submodule' do
+    skip 'Not implemented yet'
+  end
+
+  it 'shows submodule changes in the parent repo' do
+    skip 'Not implemented yet'
+  end
+
+  it 'shows dirty submodule changes' do
+    skip 'Not implemented yet'
+  end
+end
