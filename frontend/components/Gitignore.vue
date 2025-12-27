deb20930bd93dab36a4c52542fae3046d73458b9
Yoann Lecuyer <yoann.lecuyer@ringcentral.com>
2025-12-27 23:12:47 +0100

    gitignore

--- frontend/components/Gitignore.vue
+++ frontend/components/Gitignore.vue
@@ -1,13 +1,33 @@
 <template>
-    <div class="branch">
+    <div>
       <button type="button" class="btn-outline" @click.prevent="editGitignore" data-aid="edit-gitignore-button">
         <Octicon name="file" />
         <span>.gitignore</span>
       </button>
+
+      <dialog ref="alertDialog" class="dialog" aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+        <div>
+          <header>
+            <h2 id="alert-dialog-title">{{ title }}</h2>
+          </header>
+          
+          <section>
+          <textarea class="textarea" spellcheck="false"
+            style="height: 250px; font-family: monospace; resize: vertical;" v-model="gitignoreContent"></textarea>
+          </section>
+
+          <footer>
+            <button class="btn-outline" @click="alertDialog.close()">Cancel</button>
+            <button class="btn-primary" @click="saveGitignore">Save</button>
+          </footer>
+        </div>
+      </dialog>
+
     </div>
 </template>
 
 <script setup>
+import { ref, computed } from 'vue';
 import Octicon from './Octicon.vue';
 import programEvents from '/source/js/program-events.js';
 import components from '/source/js/components.js';
@@ -15,36 +35,23 @@ import components from '/source/js/components.js';
 defineOptions({ name: 'Gitignore' })
 
 const props = defineProps(['repoPath'])
+const gitignoreContent = ref(null);
+const alertDialog = ref(null);
 
-const editGitignore = () => {
-  return ungit.server
-    .getPromise('/gitignore', { path: props.repoPath })
-    .then((res) => {
-      return components.showModal('texteditmodal', {
-        title: `${props.repoPath}${ungit.config.fileSeparator}.gitignore`,
-        content: res.content,
-        closeFunc: (isYes) => {
-          if (isYes) {
-            ungit.server.putPromise('/gitignore', {
-              path: props.repoPath,
-              data: document.querySelector('dialog .text-area-content').value,
-            });
-          }
-        },
-      });
-    })
-    .catch((e) => {
-      // Not a git error but we are going to treat like one
-      programEvents.dispatch({
-        event: 'git-error',
-        data: {
-          command: `fs.write "${props.repoPath}${ungit.config.fileSeparator}.gitignore"`,
-          error: e.message || e.errorSummary,
-          stdout: '',
-          stderr: e.stack,
-          repoPath: props.repoPath,
-        },
-      });
-    });
+const title = computed(() => {
+  return `${props.repoPath}${ungit.config.fileSeparator}.gitignore`;
+});
+
+const saveGitignore = async () => {
+  await ungit.server.putPromise('/gitignore', {
+    path: props.repoPath,
+    data: gitignoreContent.value,
+  });
+  alertDialog.value.close();
+};
+
+const editGitignore = async () => {
+  gitignoreContent.value = (await ungit.server.getPromise('/gitignore', { path: props.repoPath })).content;
+  alertDialog.value.showModal()
 }
 </script>
