3f3155d1d5dacaad7b12a8d5ee8e81f06382b6a5
Yoann Lecuyer <yoann.lecuyer@ringcentral.com>
2025-12-30 18:46:22 +0100

    dark mode

--- frontend/source/js/main.js
+++ frontend/source/js/main.js
@@ -213,6 +213,10 @@ function start() {
     windowTitle.update();
   });
 
+  if (matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.classList.add('dark');
+  }
+
   navigation.init();
 };
 
