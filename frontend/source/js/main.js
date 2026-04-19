9861175b0fd60d75534cd92440449d814fd010f4
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2026-04-19 22:48:37 +0200

    remove crash

--- frontend/source/js/main.js
+++ frontend/source/js/main.js
@@ -1,7 +1,6 @@
 import '/components/app/app.js';
 import '/components/commit/commit.js';
 import '/components/commitdiff/commitdiff.js';
-import '/components/crash/crash.js';
 import '/components/graph/graph.js';
 import '/components/imagediff/imagediff.js';
 import '/components/login/login.js';
