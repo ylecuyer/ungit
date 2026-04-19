83507150e8566cdf870d0e75a0d73b6f08093b18
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2026-04-19 22:15:12 +0200

    remove stash

--- frontend/source/js/main.js
+++ frontend/source/js/main.js
@@ -9,7 +9,6 @@ import '/components/login/login.js';
 import '/components/modals/modals.js';
 import '/components/path/path.js';
 import '/components/repository/repository.js';
-import '/components/stash/stash.js';
 import '/components/textdiff/textdiff.js';
 
 import $ from 'jquery';
